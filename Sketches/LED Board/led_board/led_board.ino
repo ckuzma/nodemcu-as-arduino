c679582a3b80d97c89b5e9636eb07025f0aae57e
Christopher Kuzma <chris.kuzma@gmail.com>
2017-02-08 20:59:32 -0500

    Added Alexa control for hands-free use

--- Sketches/LED Board/led_board/led_board.ino
+++ Sketches/LED Board/led_board/led_board.ino
@@ -1,29 +1,93 @@
 #include <Adafruit_NeoPixel.h>
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <functional>
+#include <WiFiManager.h>
+#include <WiFiUdp.h>
 
+// Some UDP / WeMo specific variables:
+String device_name = "colorful artwork";  // Name of device
+float power = true;                        // Pin to toggle
+WiFiUDP UDP;
+IPAddress ipMulti(239, 255, 255, 250);
+unsigned int portMulti = 1900; // local port to listen on
+ESP8266WebServer HTTP(80);
+String serial; // Where we save the string of the UUID
+String persistent_uuid; // Where we save some socket info with the UUID
+
+// Buffer to save incoming packets:
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+
+// Define the connections and instantiate the NeoPixels
 #define PIN1 D5
 #define PIN2 D6
-
-int top[5][10][3];
-int bottom[5][10][3];
-
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(50, PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(50, PIN2, NEO_GRB + NEO_KHZ800);
 
+// Define the size of the arrays
+int top[5][10][3];
+int bottom[5][10][3];
+
+// Variables to store time counts
+int lightTimer = 0;
+int webTimer = 0;
+
 void setup() {
-  // Initialize the display, turn off
+  // Set the UUIDs and socket information:
+  prepareIds();
+
+  // Get settings from WiFi Manager:
+  WiFiManager wifiManager;
+  // wifiManager.resetSettings(); // Uncomment this to test WiFi Manager function
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.autoConnect();
+
+  // Connect to UDP:
+  bool udpConnected = connectUDP();
+  if (udpConnected){
+    startHttpServer(); // Start the HTTP Server
+  }
+
+  // Initialize the display
   strip1.begin();
   strip2.begin();
-  strip1.show();
-  strip2.show();
+
   // Initialize the random seed
   randomSeed(1989);
 }
 
 void loop() {
-  randomFill(false);
-  displayBoard1();
-  displayBoard2();
-  delay(500); // 0.5 seconds
+  if (lightTimer == 0) {
+    lightTimer = millis();
+  }
+  if (millis() - lightTimer >= 500) {
+    randomFill(power);
+    displayBoard1();
+    displayBoard2();
+    lightTimer = 0;
+  }
+  HTTP.handleClient();
+  if (webTimer == 0) {
+    webTimer = millis();
+  }
+  if (millis() - webTimer >= 1) {
+    // If there are packets, we parse them:
+    int packetSize = UDP.parsePacket();
+    if(packetSize) {
+      int len = UDP.read(packetBuffer, 255);
+      if (len > 0) {
+        packetBuffer[len] = 0;
+      }
+      String request = packetBuffer;
+      if(request.indexOf('M-SEARCH') > 0) {
+        if(request.indexOf("urn:Belkin:device:**") > 0) {
+          respondToSearch();
+        }
+      }
+    }
+    webTimer = 0;
+  }
 }
 
 void randomFill(bool fill) {
@@ -61,4 +125,135 @@ void displayBoard2() {
   strip2.show();
 }
 
+void prepareIds() {
+  uint32_t chipId = ESP.getChipId();
+  char uuid[64];
+  sprintf_P(uuid, PSTR("38323636-4558-4dda-9188-cda0e6%02x%02x%02x"),
+  (uint16_t) ((chipId >> 16) & 0xff),
+  (uint16_t) ((chipId >>  8) & 0xff),
+  (uint16_t)   chipId        & 0xff);
 
+  serial = String(uuid);
+  persistent_uuid = "Socket-1_0-" + serial;
+}
+
+bool connectUDP(){
+  boolean state = false;
+
+  if(UDP.beginMulticast(WiFi.localIP(), ipMulti, portMulti)) {
+    state = true;
+  }
+
+  return state;
+}
+
+void startHttpServer() {
+  HTTP.on("/index.html", HTTP_GET, [](){
+    HTTP.send(200, "text/plain", "Hello World!");
+  });
+
+  HTTP.on("/upnp/control/basicevent1", HTTP_POST, []() {
+    String request = HTTP.arg(0);
+    if(request.indexOf("<BinaryState>1</BinaryState>") > 0) {
+      power = true;
+    }
+    if(request.indexOf("<BinaryState>0</BinaryState>") > 0) {
+      power = false;
+    }
+    HTTP.send(200, "text/plain", "");
+  });
+
+  HTTP.on("/eventservice.xml", HTTP_GET, [](){
+    String eventservice_xml = "<?scpd xmlns=\"urn:Belkin:service-1-0\"?>"
+    "<actionList>"
+    "<action>"
+    "<name>SetBinaryState</name>"
+    "<argumentList>"
+    "<argument>"
+    "<retval/>"
+    "<name>BinaryState</name>"
+    "<relatedStateVariable>BinaryState</relatedStateVariable>"
+    "<direction>in</direction>"
+    "</argument>"
+    "</argumentList>"
+    "<serviceStateTable>"
+    "<stateVariable sendEvents=\"yes\">"
+    "<name>BinaryState</name>"
+    "<dataType>Boolean</dataType>"
+    "<defaultValue>0</defaultValue>"
+    "</stateVariable>"
+    "<stateVariable sendEvents=\"yes\">"
+    "<name>level</name>"
+    "<dataType>string</dataType>"
+    "<defaultValue>0</defaultValue>"
+    "</stateVariable>"
+    "</serviceStateTable>"
+    "</action>"
+    "</scpd>\r\n"
+    "\r\n";
+
+    HTTP.send(200, "text/plain", eventservice_xml.c_str());
+  });
+
+  HTTP.on("/setup.xml", HTTP_GET, [](){
+    IPAddress localIP = WiFi.localIP();
+    char s[16];
+    sprintf(s, "%d.%d.%d.%d", localIP[0], localIP[1], localIP[2], localIP[3]);
+
+    String setup_xml = "<?xml version=\"1.0\"?>"
+    "<root>"
+    "<device>"
+    "<deviceType>urn:Belkin:device:controllee:1</deviceType>"
+    "<friendlyName>"+ device_name +"</friendlyName>"
+    "<manufacturer>Belkin International Inc.</manufacturer>"
+    "<modelName>Emulated Socket</modelName>"
+    "<modelNumber>3.1415</modelNumber>"
+    "<UDN>uuid:"+ persistent_uuid +"</UDN>"
+    "<serialNumber>221517K0101769</serialNumber>"
+    "<binaryState>0</binaryState>"
+    "<serviceList>"
+    "<service>"
+    "<serviceType>urn:Belkin:service:basicevent:1</serviceType>"
+    "<serviceId>urn:Belkin:serviceId:basicevent1</serviceId>"
+    "<controlURL>/upnp/control/basicevent1</controlURL>"
+    "<eventSubURL>/upnp/event/basicevent1</eventSubURL>"
+    "<SCPDURL>/eventservice.xml</SCPDURL>"
+    "</service>"
+    "</serviceList>"
+    "</device>"
+    "</root>\r\n"
+    "\r\n";
+
+    HTTP.send(200, "text/xml", setup_xml.c_str());
+  });
+
+  HTTP.begin();
+}
+
+void respondToSearch() {
+  IPAddress localIP = WiFi.localIP();
+  char s[16];
+  sprintf(s, "%d.%d.%d.%d", localIP[0], localIP[1], localIP[2], localIP[3]);
+
+  String response =
+  "HTTP/1.1 200 OK\r\n"
+  "CACHE-CONTROL: max-age=86400\r\n"
+  "DATE: Wed, 8 Feb 2017 02:30:00 GMT\r\n"
+  "EXT:\r\n"
+  "LOCATION: http://" + String(s) + ":80/setup.xml\r\n"
+  "OPT: \"http://schemas.upnp.org/upnp/1/0/\"; ns=01\r\n"
+  "01-NLS: b9200ebb-736d-4b93-bf03-835149d13983\r\n"
+  "SERVER: Unspecified, UPnP/1.0, Unspecified\r\n"
+  "ST: urn:Belkin:device:**\r\n"
+  "USN: uuid:" + persistent_uuid + "::urn:Belkin:device:**\r\n"
+  "X-User-Agent: redsonic\r\n\r\n";
+
+  UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+  UDP.write(response.c_str());
+  UDP.endPacket();
+}
+
+void configModeCallback(WiFiManager *myWiFiManager) {
+  WiFi.softAPIP();
+  myWiFiManager->getConfigPortalSSID();
+}
