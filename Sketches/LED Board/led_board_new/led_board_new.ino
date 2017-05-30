d5801ba303cc871ffd7d88e229ab522e0e6021b3
Christopher Kuzma <chris.kuzma@gmail.com>
2017-05-30 11:54:24 -0400

    Updated Watson w/animation

--- Sketches/LED Board/led_board_new/led_board_new.ino
+++ Sketches/LED Board/led_board_new/led_board_new.ino
@@ -52,35 +52,156 @@ void changePixel(int x, int y, int r, int g, int b) {
 }
 
 void drawWatson() {
+  // Draw on
   changePixel(5, 0, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(2, 1, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(5, 1, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(8, 1, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(3, 2, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(5, 2, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(7, 2, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(0, 3, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(9, 3, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(1, 4, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(4, 4, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(5, 4, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(8, 4, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(3, 5, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(6, 5, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(2, 6, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(7, 6, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(2, 7, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(7, 7, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(3, 8, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(6, 8, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(4, 9, 31, 154, 236);
+  displayBoard();
+  delay(80);
   changePixel(5, 9, 31, 154, 236);
+  displayBoard();
+  delay(80);
+  
+  // Pause
+  delay(5000);
+  
+  // Draw out
+  changePixel(5, 0, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(2, 1, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(5, 1, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(8, 1, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(3, 2, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(5, 2, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(7, 2, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(0, 3, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(9, 3, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(1, 4, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(4, 4, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(5, 4, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(8, 4, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(3, 5, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(6, 5, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(2, 6, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(7, 6, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(2, 7, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(7, 7, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(3, 8, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(6, 8, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(4, 9, 0, 0, 0);
+  displayBoard();
+  delay(80);
+  changePixel(5, 9, 0, 0, 0);
+  displayBoard();
+  delay(80);
+
+  // Pause
+  delay(5000);
 }
 
 void loop() {
   // Set some arbitrary values...
   drawWatson();
-  
-  // Display it!
-  displayBoard();
 }
