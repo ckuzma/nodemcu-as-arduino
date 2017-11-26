3b2b55221ee9491d3633ba901b0f3c5d4e5b5a43
Unknown <chris.kuzma@gmail.com>
2017-11-25 23:10:04 -0800

    Removed all OTA programmability, moved display flip to right spot

--- Sketches/OLED/WeatherStation/WeatherStation.ino
+++ Sketches/OLED/WeatherStation/WeatherStation.ino
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 #include <JsonListener.h>
-#include <ArduinoOTA.h>
 #include <ESP8266mDNS.h>
 
 #include <DNSServer.h>
@@ -91,7 +90,6 @@ void setup() {
   display.clear();
   display.display();
 
-  //display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setContrast(255);
@@ -146,12 +144,7 @@ void setup() {
 
   // Inital UI takes care of initalising the display too.
   ui.init();
-
-  // Setup OTA
-  Serial.println("Hostname: " + hostname);
-  ArduinoOTA.setHostname((const char *)hostname.c_str());
-  ArduinoOTA.onProgress(drawOtaProgress);
-  ArduinoOTA.begin();
+  //display.flipScreenVertically(); // If we need to flip display
 
   updateData(&display);
 
